4ce379210a19c101947b4a850c606ecc51368fbe
Rodrigo Gomes Calixto da Silva <rodrigo.gomes@gmail.com>
2022-05-10 18:49:11 -0300

    adicionando lógica no controller

---
+++ controllers/tabuada.js
@@ -0,0 +1,29 @@
+const list = (request, response) =>{
+    const numeros = []
+    for(let i=1; i<=100; i++){
+        numeros.push(i)
+    }
+    response.render('tabuada/list', {numeros})
+}
+
+const tabuada = (request, response) =>{
+    const tabuadas = []
+    const num = request.params.num
+
+    for(let i=0; i<=100; i++){
+        tabuadas.push({
+            num,
+            i,
+            resultado: num*i
+        })
+    }
+
+    response.render('tabuada/tabuada', {
+        num: request.params.num,
+        tabuadas
+    })
+}
+
+module.exports = {
+    list, tabuada
+}
